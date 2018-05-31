76b33a09588cbf873984fee8e15a7d997f0510b0
Are Edvardsen <are.edvardsen@helse-nord.no>
2018-05-31 10:59:56 +0200

    init commit meta doc

---
+++ R/raptools.R
@@ -0,0 +1,10 @@
+#' raptools: A package containing development tools at Rapporteket.
+#'
+#'  Provide functions and resources for development tasks at Rapporteket.
+#'
+#' @section raptools functions:
+#' static_help
+#'
+#' @docType package
+#' @name raptools
+NULL
