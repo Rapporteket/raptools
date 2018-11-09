4c215ce0b4540f9298a52d6c7ae60b66cca49130
Are Edvardsen <are.edvardsen@helse-nord.no>
2018-11-09 08:16:43 +0100

    bug

--- inst/shinyApps/makeUrl/ui.R
+++ inst/shinyApps/makeUrl/ui.R
@@ -15,7 +15,7 @@ ui <- fluidPage(
       selectInput(inputId = "app",
                 label = "app:",
                 choices = c("", "/makeUrl", "/intensiv", "/nakke", "/noric",
-                            "/muskel", "RapShinyTemplate")),
+                            "/muskel", "/RapShinyTemplate")),
       selectInput(inputId = "user",
                   label = "user:",
                   choices = c("testUser1", "testUser2")),
