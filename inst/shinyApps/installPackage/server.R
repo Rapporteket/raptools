25d23d05c8170018618ab6f3a0d7a80e953b4629
Are Edvardsen <biorakel@gmail.com>
2019-12-05 14:04:21 +0100

    extending with current user data

--- inst/shinyApps/installPackage/server.R
+++ inst/shinyApps/installPackage/server.R
@@ -19,4 +19,49 @@ server <- function(input, output, session) {
   # test environ vars
   output$confPath <- renderPrint(paste("Config path:", Sys.getenv("R_RAP_CONFIG_PATH")))
 
+  # Various calls for session data from rapbase and systemn settings
+  output$callUser <- renderText({
+    paste("rapbase::getUserName(session):",
+          rapbase::getUserName(session))
+  })
+  output$callGroups <- renderText({
+    paste("rapbase::getUserGroups(session):",
+          rapbase::getUserGroups(session))
+  })
+  output$callReshId <- renderText({
+    paste("rapbase::getUserReshId(session):",
+          rapbase::getUserReshId(session))
+  })
+  output$callRole <- renderText({
+    paste("rapbase::getUserRole(session):",
+          rapbase::getUserRole(session))
+  })
+
+  output$callEmail <- renderText({
+    paste("rapbase::getUserEmail(session):",
+          rapbase::getUserEmail(session))
+  })
+
+  output$callFullName <- renderText({
+    paste("rapbase::getUserFullName(session):",
+          rapbase::getUserFullName(session))
+  })
+
+  output$callPhone <- renderText({
+    paste("rapbase::getUserPhone(session):",
+          rapbase::getUserPhone(session))
+  })
+
+  output$envInstance <- renderText({
+    Sys.getenv("R_RAP_INSTANCE")
+  })
+
+  output$envConfigPath <- renderText({
+    Sys.getenv("R_RAP_CONFIG_PATH")
+  })
+
+  output$locale <- renderText({
+    Sys.getlocale()
+  })
+
 }
