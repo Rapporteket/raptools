1234202dc1ebafdc490a9d0146f95c84aee17555
Are Edvardsen <biorakel@gmail.com>
2019-12-05 13:57:42 +0100

    amp and extend gui

--- inst/shinyApps/installPackage/ui.R
+++ inst/shinyApps/installPackage/ui.R
@@ -1,39 +1,62 @@
-# Define UI for package install app ----
-ui <- fluidPage(
+addResourcePath('rap', system.file('www', package='rapbase'))
+appTitle = "Swiss army knife"
 
+
+ui <- tagList(
   shinyjs::useShinyjs(),
+  navbarPage(
+    title = div(a(includeHTML(system.file('www/logo.svg', package='rapbase'))),
+                appTitle),
+    windowTitle = appTitle,
+    theme = "rap/bootstrap.css",
 
-  # App title ----
-  titlePanel("Install package from GitHub"),
+    tabPanel("Install package from GitHub",
+      # Sidebar layout with a input and output definitions ----
+      sidebarLayout(
+      # Sidebar panel for inputs ----
+        sidebarPanel(
+          selectInput(inputId = "package",
+                      label = "Package:",
+                      choices = c("intensiv", "muskel", "Nakke", "nger",
+                                  "nordicscir", "norgast", "NORIC", "nordummy", "rapbase",
+                                  "rapgen", "raptools", "raplog", "hisreg")),
+          selectInput(inputId = "branch",
+                      label = "Branch:",
+                      choices = c("shinyfy", "rel", "master", "yt")),
+          actionButton(inputId = "install",
+                       label = "Install")
+        ),
+        mainPanel(
+        # output
+          p(em("System message:")),
+          verbatimTextOutput("sysMessage"),
+          p(em("Function message:")),
+          verbatimTextOutput("funMessage"),
+          p(em("Test env var:")),
+          verbatimTextOutput("confPath")
 
-  # Sidebar layout with a input and output definitions ----
-  sidebarLayout(
-
-    # Sidebar panel for inputs ----
-    sidebarPanel(
-      selectInput(inputId = "package",
-                label = "Package:",
-                choices = c("intensiv", "muskel", "Nakke", "nger",
-                            "nordicscir", "norgast", "NORIC", "nordummy", "rapbase",
-                            "rapgen", "raptools", "raplog", "hisreg")),
-      selectInput(inputId = "branch",
-                  label = "Branch:",
-                  choices = c("shinyfy", "rel", "master", "yt")),
-      actionButton(inputId = "install",
-                   label = "Install")
+        )
+      )
     ),
-
-    # Main panel for displaying outputs ----
-    mainPanel(
-
-      # output
-      p(em("System message:")),
-      verbatimTextOutput("sysMessage"),
-      p(em("Function message:")),
-      verbatimTextOutput("funMessage"),
-      p(em("Test env var:")),
-      verbatimTextOutput("confPath")
-
+    tabPanel("System info",
+      mainPanel(
+        # return from rapbase functions
+        h4("Test 'rapbase' functions using the session object:"),
+        textOutput("callUser"),
+        textOutput("callGroups"),
+        textOutput("callReshId"),
+        textOutput("callRole"),
+        textOutput("callEmail"),
+        textOutput("callFullName"),
+        textOutput("callPhone"),
+        h4("Environment var R_RAP_INSTANCE:"),
+        textOutput("envInstance"),
+        h4("Environmental var R_RAP_CONFIG_PATH:"),
+        textOutput("envConfigPath"),
+        h4("Locale settings:"),
+        textOutput("locale")
+      )
     )
-  )
-)
+  ) # navbarPage
+) # tagList
+
