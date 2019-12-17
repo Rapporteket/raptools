482dfd4be0195f095adb86756e51f1ca7fc2786f
Are Edvardsen <biorakel@gmail.com>
2019-12-17 11:32:51 +0100

    add registry

--- inst/shinyApps/installPackage/ui.R
+++ inst/shinyApps/installPackage/ui.R
@@ -21,7 +21,7 @@ ui <- tagList(
                       label = "Package:",
                       choices = c("intensiv", "muskel", "Nakke", "nger",
                                   "nordicscir", "norgast", "NORIC", "nordummy", "rapbase",
-                                  "rapgen", "raptools", "raplog", "hisreg")),
+                                  "rapgen", "raptools", "raplog", "hisreg", "norspis")),
           selectInput(inputId = "branch",
                       label = "Branch:",
                       choices = c("shinyfy", "rel", "master", "yt")),
