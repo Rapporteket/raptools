d44ce3674faf1c06318e91752a2235ed03be3562
yte0 <Yohannes.Tesfay@helse-nord.no>
2020-01-07 15:12:12 +0100

    minor correction

--- tests/testthat/test-calendarAutoReport.R
+++ tests/testthat/test-calendarAutoReport.R
@@ -4,7 +4,7 @@ test_that("class of the output object is ggplot", {
   expect_equal(class(raptools::calendarAutoReport(1:10)),c("gg","ggplot"))
 })
 
-test_that("an error is produced for wrong input types", {
+test_that("errors are produced for wrong input types", {
   expect_error(
     raptools::calendarAutoReport(
       runDayOfYear = "error")
