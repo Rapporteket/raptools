0aabf40bb9c411098d7e83318ae4c46164f2359c
Are Edvardsen <biorakel@gmail.com>
2020-01-10 09:01:10 +0100

    proper data in package

---
+++ R/data.R
@@ -0,0 +1,25 @@
+#' Checklist when deploying application
+#'
+#' A dataset containing checklist entries used prior to deploying a registry
+#' app at Rapporteket
+#'
+#' @format A named list of oks
+#' \describe{
+#'   \item{name}{the text of each check in a PRODUCTION environment}
+#'   \item{values}{dummy ok, not used}
+#'   ...
+#' }
+"prodChecklist"
+
+#' Checklist when deploying application
+#'
+#' A dataset containing checklist entries used prior to deploying a registry
+#' app at Rapporteket
+#'
+#' @format A named list of oks
+#' \describe{
+#'   \item{name}{the text of each check in a QA environment}
+#'   \item{values}{dummy ok, not used}
+#'   ...
+#' }
+"qaChecklist"
