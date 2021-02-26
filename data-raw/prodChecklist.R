f6ea24cf3c4888add88170d28cda9d1479c6ebcf
Are Edvardsen <biorakel@gmail.com>
2021-02-26 14:50:14 +0100

    update checklists

--- data-raw/prodChecklist.R
+++ data-raw/prodChecklist.R
@@ -2,10 +2,9 @@
 
 prodChecklist <- list(`sett at alt fungere som det skal i QA?`="ok",
                       `faatt godkjenning fra registeret?`="ok",
-                      `tatt over alle relevante endringer i 'master'?`="ok",
                       `faatt en kollega til aa se over alle endringer?`="ok",
                       `fjernet alle avhengigheter (Imports) til pakker som ikke er produksjonsklare`="ok",
                       `oppdatert versjonsnummer i DESCRIPTION`="ok",
-                      `tilsvarende tagget versjonen i git som skal legges ut`="ok")
+                      `dokumentert endringer siden forrige versjon`="ok")
 
 usethis::use_data(`prodChecklist`, overwrite = TRUE)
--- data-raw/qaChecklist.R
+++ data-raw/qaChecklist.R
@@ -2,7 +2,6 @@
 
 qaChecklist <- list(`testet at alt fungere som det skal`="ok",
                     `faatt godkjenning fra registeret`="ok",
-                    `tatt over alle relevante endringer i 'rel' eller master'`="ok",
                     `fjernet alle avhengigheter (Imports) til pakker som ikke er produksjonsklare`="ok",
                     `oppdatert versjonsnummer i DESCRIPTION`="ok")
 
